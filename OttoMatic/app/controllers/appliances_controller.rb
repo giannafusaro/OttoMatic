51b11fa4144a34f540781e92e74ae6358b58b034
Gianna Fusaro <giannafusaro389@gmail.com>
2015-08-04 20:21:22 -0700

    add appliances controller and crud

---
+++ OttoMatic/app/controllers/appliances_controller.rb
@@ -0,0 +1,37 @@
+class AppliancesController < ApplicationController
+  def index
+    @appliances = Appliance.all
+  end
+  def new
+    @appliance = Appliance.new
+  end
+  def create
+    @appliance = Appliance.new post_params
+    if @appliance.save
+      flash[:notice] = "Appliance created successfully!"
+    else
+      flash[:notice] = "Sorry, something went wrong."
+    end
+
+    render html: render_to_string(partial: "form").html_safe
+  end
+  def edit
+    @appliance = Appliance.find params[:id]
+  end
+  def update
+    @appliance = Appliance.find params[:id]
+    if @appliance.update_attributes post_params
+      flash[:notice] = "Appliance created successfully!"
+    else
+      flash[:notice] = "Sorry, something went wrong."
+    end
+    render html: render_to_string(partial: "form").html_safe
+  end
+  def destroy
+  end
+  private
+
+    def post_params
+      params.require(:appliance).permit(:id, :brand, :model, :serial, :purchased_at)
+    end
+end
