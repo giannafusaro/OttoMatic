c8f15959096d91ee4714c4a98636b559d41e7e58
Gianna Fusaro <giannafusaro389@gmail.com>
2015-08-07 02:15:42 -0700

    use datetimepicker plug-in with many customizations for scheduling jobs and choosing date of purchase

--- OttoMatic/app/assets/javascripts/application.js
+++ OttoMatic/app/assets/javascripts/application.js
@@ -11,6 +11,9 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
-//= require turbolinks
+//= require jquery-ui.min
+//= require moment
+//= require bootstrap-datetimepicker
+
+//= require_tree ../../../vendor/assets/javascripts/.
 //= require_tree .
